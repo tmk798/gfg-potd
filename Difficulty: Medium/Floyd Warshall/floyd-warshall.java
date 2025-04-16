7f5d6dcf443174d3418b7b551f6effab4788a966
Tanmay. M. Khule <150503993+tmk798@users.noreply.github.com>
2025-04-16 16:41:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial template for JAVA
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            int[][] matrix = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                String[] s = br.readLine().trim().split(" ");
+                for (int j = 0; j < n; j++) matrix[i][j] = Integer.parseInt(s[j]);
+            }
+            Solution obj = new Solution();
+            obj.floydWarshall(matrix);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function template for JAVA
+class Solution
+{
+    public void floydWarshall(int[][] matrix)
+    {
+        int n=matrix.length;
+        int max=100000000;
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(matrix[i][k]==max || matrix[k][j]==max) continue;
+                    matrix[i][j]=Math.min(matrix[i][j],matrix[i][k]+matrix[k][j]);
+                }
+            }
+        }
+        
+    }
+}
