c29fc4846fd5465eaa764b489b3b3ebdda52af66
Tanmay. M. Khule <150503993+tmk798@users.noreply.github.com>
2025-04-22 14:27:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Unique Number I/unique-number-i.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        while (t-- > 0) {
+            String[] arr1Str = sc.nextLine().split(" ");
+            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
+            Solution ob = new Solution();
+            int ans = ob.findUnique(arr);
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+class Solution {
+    public int findUnique(int[] arr) {
+        // code here
+        HashMap<Integer,Integer> mp=new HashMap<>();
+        for(int i=0;i<arr.length;i++)
+        {
+            mp.put(arr[i],mp.getOrDefault(arr[i],0)+1);
+        }
+        
+        for(int key:mp.keySet())
+        {
+            if(mp.get(key)==1)
+            return key;
+        }
+        
+        return -1;
+    }
+}
